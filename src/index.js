6efedc542ce97815347c39c78ae0600fa4c98187
Nikolay <nikoobox@gmail.com>
2020-12-07 15:10:17 -0500

    npm run webpach build

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import "./styles/index.scss";
 import "./styles/reset.scss";
 import "./styles/navbar.scss";
 import "./styles/navbar.scss";
+import './styles/main.scss';
 
 import Test from './scripts/test';
