18caddb0d97b30a694256baa83f52ec6b906cf5f
Nikolay <nikoobox@gmail.com>
2022-10-10 20:08:57 -0400

    make season update automatic

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const path = require("path");
 const fetch = require("node-fetch");
 require("dotenv").config();
 
+// to run app
+// npm start app
+// http://localhost:8000/
+
 const PORT = process.env.PORT || 8000; // process.env accesses heroku's environment variables
+
+const dateYear = new Date().getFullYear();
+const mon = new Date().getMonth();
+const dateYearNorm = dateYear % 2 === 0 && mon >= 8 ? dateYear : dateYear - 1;
 
 app.use(express.static("dist"));
 
@@ -12,9 +20,30 @@ app.get("/", (request, res) => {
   res.sendFile(path.join(__dirname, "./dist/index.html"));
 });
 
+app.get("/leagues/seasons", (req, res) => {
+  fetch(`https://v3.football.api-sports.io`, {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "v3.football.api-sports.io",
+      "x-rapidapi-key": `${process.env.API_KEY}`,
+    },
+  })
+    .then((response) => {
+      console.log("season is: ", season);
+      return response.text();
+    })
+    .then((body) => {
+      let results = JSON.parse(body);
+      res.send(results); // sends to frontend
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 app.get("/currentround", (req, res) => {
   fetch(
-    "https://v3.football.api-sports.io/fixtures/rounds?season=2022&league=39&current=true",
+    `https://v3.football.api-sports.io/fixtures/rounds?season=${dateYearNorm}&league=39&current=true`,
     {
       method: "GET",
       headers: {
@@ -24,7 +53,6 @@ app.get("/currentround", (req, res) => {
     }
   )
     .then((response) => {
-      console.log("response.text()", response.text());
       return response.text();
     })
     .then((body) => {
@@ -39,7 +67,7 @@ app.get("/currentround", (req, res) => {
 app.get("/games", (req, res) => {
   console.log(req.query);
   fetch(
-    `https://v3.football.api-sports.io/fixtures?league=39&season=2022&round=${req.query.round}`,
+    `https://v3.football.api-sports.io/fixtures?league=39&season=${dateYearNorm}&round=${req.query.round}`,
     {
       method: "GET",
       headers: {
@@ -61,13 +89,16 @@ app.get("/games", (req, res) => {
 });
 
 app.get("/standings", (req, res) => {
-  fetch("https://v3.football.api-sports.io/standings?league=39&season=2022", {
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "v3.football.api-sports.io",
-      "x-rapidapi-key": `${process.env.API_KEY}`,
-    },
-  })
+  fetch(
+    `https://v3.football.api-sports.io/standings?league=39&season=${dateYearNorm}`,
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "v3.football.api-sports.io",
+        "x-rapidapi-key": `${process.env.API_KEY}`,
+      },
+    }
+  )
     .then((response) => {
       return response.text();
     })
@@ -96,7 +127,6 @@ app.get("/game", (req, res) => {
     })
     .then((body) => {
       let results = JSON.parse(body);
-      // console.log(results); // logs to server
       res.send(results); // sends to frontend
     })
     .catch((err) => {
--- src/scripts/games.js
+++ src/scripts/games.js
@@ -1,25 +1,27 @@
 import axios from "axios";
-import renderGameById from './game';
-import renderVotingModal from './renderVotingModal';
-import gamesRefresh from './gamesRefresh';
-import eplLogo from '../../dist/images/blacklogo.png';
+import renderGameById from "./game";
+import renderVotingModal from "./renderVotingModal";
+import gamesRefresh from "./gamesRefresh";
+import eplLogo from "../../dist/images/blacklogo.png";
 
 const renderGames = (round) => {
-    const allGamesDiv = document.getElementById("all-games")
-    const allGamesBoxDiv = document.createElement("div");
-    allGamesDiv.innerHTML = "<div></div>";
-    const leagueInfoDiv = document.getElementById("league-info");
-    leagueInfoDiv.classList.add('left');
+  const allGamesDiv = document.getElementById("all-games");
+  const allGamesBoxDiv = document.createElement("div");
+  allGamesDiv.innerHTML = "<div></div>";
+  const leagueInfoDiv = document.getElementById("league-info");
+  leagueInfoDiv.classList.add("left");
 
-    axios.get(`./games?round=${round}`).then((res) => {
-        const fixturesArr = res.data.response;
-        const sectionNameDiv = document.createElement("div");
-        sectionNameDiv.classList.add("section-name");
-        
-        let liveGamesStatus = false;
+  axios
+    .get(`./games?round=${round}`)
+    .then((res) => {
+      const fixturesArr = res.data.response;
+      const sectionNameDiv = document.createElement("div");
+      sectionNameDiv.classList.add("section-name");
 
-        const nextSeason = res.data.response[0].league.season + 1;
-        leagueInfoDiv.innerHTML = `
+      let liveGamesStatus = false;
+
+      const nextSeason = res.data.response[0].league.season + 1;
+      leagueInfoDiv.innerHTML = `
         <div class='league-box'>
             <div class='country-box'>
                 <div class='name'>${res.data.response[0].league.country}</div>
@@ -51,13 +53,13 @@ const renderGames = (round) => {
             </div>
         </div>
         `;
-        let currentRoundString = res.data.response[0].league.round;
-        let currentRound = `${currentRoundString.split(' ').pop()} `;
-        let prevRound = parseInt(currentRound) - 1;
-        let nextRound = parseInt(currentRound) + 1;
-        sectionNameDiv.innerHTML = `
+      let currentRoundString = res.data.response[0].league.round;
+      let currentRound = `${currentRoundString.split(" ").pop()} `;
+      let prevRound = parseInt(currentRound) - 1;
+      let nextRound = parseInt(currentRound) + 1;
+      sectionNameDiv.innerHTML = `
             <div class='roundPrev'>
-                <i class="fas fa-chevron-left"></i> GO TO ROUND ${ prevRound}
+                <i class="fas fa-chevron-left"></i> GO TO ROUND ${prevRound}
             </div>
             <div class='roundCurr'>
                  ALL GAMES - ROUND ${currentRound} 
@@ -65,58 +67,65 @@ const renderGames = (round) => {
             <div class='roundNext'>
                 GO TO ROUND ${nextRound} <i class="fas fa-chevron-right"></i>
             </div>
-            `
-        allGamesDiv.append(sectionNameDiv);
+            `;
+      allGamesDiv.append(sectionNameDiv);
 
-        const pRound = `Regular Season - ${prevRound}`;
-        const nRound = `Regular Season - ${nextRound}`;
-        const previousRound = document.querySelector(".roundPrev");
-        const followingRound = document.querySelector(".roundNext");
-        
-        prevRound !== 0 ? previousRound.addEventListener('click', () => renderGames(pRound)) : "";
-        // previousRound.addEventListener('click', () => renderGames(pRound));
-        nextRound === 38 ? '' :
-        followingRound.addEventListener('click', () => renderGames(nRound));    
-       
-        allGamesBoxDiv.classList.add("all-games-box");
-        allGamesDiv.append(allGamesBoxDiv);
-        const gameLiveStatus = ['1H','HT','2H','ET','P','BT'];
-        const gameNotLiveStatus = ['NS', 'FT'];
-        
-        fixturesArr.forEach((fix, idx) => {
-            const team1 = fix.teams.home.name;
-            const logo1 = fix.teams.home.logo;
-            const team2 = fix.teams.away.name;
-            const logo2 = fix.teams.away.logo;
-            const score1 = fix.goals.home;
-            const score2 = fix.goals.away;
-            const gameStatus = fix.fixture.status.short;
-            const gameId = fix.fixture.id;
+      const pRound = `Regular Season - ${prevRound}`;
+      const nRound = `Regular Season - ${nextRound}`;
+      const previousRound = document.querySelector(".roundPrev");
+      const followingRound = document.querySelector(".roundNext");
 
-            // to check if at leat one game is live to enable updates
-            if (gameLiveStatus.includes(gameStatus)) liveGamesStatus = true;
-          
-            const fixtureRowDiv = document.createElement('div');
-            const gameStatusRowDiv = document.createElement('div');
-            const scoreBoxDiv = document.createElement('div');
+      prevRound !== 0
+        ? previousRound.addEventListener("click", () => renderGames(pRound))
+        : "";
+      // previousRound.addEventListener('click', () => renderGames(pRound));
+      nextRound === 38
+        ? ""
+        : followingRound.addEventListener("click", () => renderGames(nRound));
 
-            fixtureRowDiv.classList.add('fixture-box');
-            gameStatusRowDiv.classList.add('game-status-row');
-            scoreBoxDiv.classList.add('score-box');
+      allGamesBoxDiv.classList.add("all-games-box");
+      allGamesDiv.append(allGamesBoxDiv);
+      const gameLiveStatus = ["1H", "HT", "2H", "ET", "P", "BT"];
+      const gameNotLiveStatus = ["NS", "FT"];
 
-            fixtureRowDiv.setAttribute('gameId', gameId );
+      fixturesArr.forEach((fix, idx) => {
+        const team1 = fix.teams.home.name;
+        const logo1 = fix.teams.home.logo;
+        const team2 = fix.teams.away.name;
+        const logo2 = fix.teams.away.logo;
+        const score1 = fix.goals.home;
+        const score2 = fix.goals.away;
+        const gameStatus = fix.fixture.status.short;
+        const gameId = fix.fixture.id;
 
-            // if game is live then show 'score', otherwise show 'vs'
-            const temp = (gameLiveStatus.includes(gameStatus) || (gameStatus === 'FT')) ? `
+        // to check if at leat one game is live to enable updates
+        if (gameLiveStatus.includes(gameStatus)) liveGamesStatus = true;
+
+        const fixtureRowDiv = document.createElement("div");
+        const gameStatusRowDiv = document.createElement("div");
+        const scoreBoxDiv = document.createElement("div");
+
+        fixtureRowDiv.classList.add("fixture-box");
+        gameStatusRowDiv.classList.add("game-status-row");
+        scoreBoxDiv.classList.add("score-box");
+
+        fixtureRowDiv.setAttribute("gameId", gameId);
+
+        // if game is live then show 'score', otherwise show 'vs'
+        const temp =
+          gameLiveStatus.includes(gameStatus) || gameStatus === "FT"
+            ? `
             <div class='score-box'>
                 <div class='score'>${score1}</div>
                 <div class='score'>${score2}</div>
             </div>
-            `: '<div class="vs">vs</div>';
+            `
+            : '<div class="vs">vs</div>';
 
-            // if game is not started, display VOTE button 
-            const voteBtnDisplay = (gameStatus === 'NS')? 
-            `<div class='voting-btn-box'>
+        // if game is not started, display VOTE button
+        const voteBtnDisplay =
+          gameStatus === "NS"
+            ? `<div class='voting-btn-box'>
                 <button 
                     class='voting-btn' 
                     team1-name='${team1}'
@@ -126,9 +135,10 @@ const renderGames = (round) => {
                     gameId=${gameId}>
                     VOTE
                 </button>
-            </div>` : '';
-            
-            fixtureRowDiv.innerHTML =  `
+            </div>`
+            : "";
+
+        fixtureRowDiv.innerHTML = `
             <div class='game-status-section'>${fix.fixture.status.long}</div>
             <div class='team1-box' >
                 <div class="name">${team1}</div>
@@ -141,51 +151,43 @@ const renderGames = (round) => {
             </div>
             ${voteBtnDisplay}
             `;
-            
-            if (gameStatus !== 'NS') {
-                fixtureRowDiv.addEventListener('click', () => renderGameById(gameId, score1, score2));
-            }
-            
-            allGamesBoxDiv.appendChild(fixtureRowDiv);
-            renderVotingModal(allGamesBoxDiv, team1, team2);
-        });
-        
-        //live update functionality
-        const buttonLiveUpdates = document.createElement("div");
-        buttonLiveUpdates.classList.add('live-updates-box');
-        allGamesBoxDiv.appendChild(buttonLiveUpdates);
 
-        if (localStorage.getItem('localStCounter') !== null){
-            let newCounter = localStorage.getItem('localStCounter');
-            newCounter ++;
-            localStorage.setItem('localStCounter', newCounter);
-            if (newCounter === 3){
-                clearInterval(localStorage.getItem('localStInterval'));
-                localStorage.clear();
-            }
+        if (gameStatus !== "NS") {
+          fixtureRowDiv.addEventListener("click", () =>
+            renderGameById(gameId, score1, score2)
+          );
         }
 
-        // if (liveGamesStatus){
-            buttonLiveUpdates.innerHTML = `
+        allGamesBoxDiv.appendChild(fixtureRowDiv);
+        renderVotingModal(allGamesBoxDiv, team1, team2);
+      });
+
+      //live update functionality
+      const buttonLiveUpdates = document.createElement("div");
+      buttonLiveUpdates.classList.add("live-updates-box");
+      allGamesBoxDiv.appendChild(buttonLiveUpdates);
+
+      if (localStorage.getItem("localStCounter") !== null) {
+        let newCounter = localStorage.getItem("localStCounter");
+        newCounter++;
+        localStorage.setItem("localStCounter", newCounter);
+        if (newCounter === 3) {
+          clearInterval(localStorage.getItem("localStInterval"));
+          localStorage.clear();
+        }
+      }
+
+      buttonLiveUpdates.innerHTML = `
             <button id='live-updates'>ACTIVATE LIVE UPDATES</button>
             <button id='no-live-updates'>STOP LIVE UPDATES</button>
             `;
-            const btnLiveUpdates = document.getElementById("live-updates");
-            const btnNoLiveUpdates = document.getElementById("no-live-updates");
+      const btnLiveUpdates = document.getElementById("live-updates");
+      const btnNoLiveUpdates = document.getElementById("no-live-updates");
 
-            gamesRefresh(btnLiveUpdates, btnNoLiveUpdates, round)
-        // }else{
-        //     buttonLiveUpdates.innerHTML = `
-        //     <button class='no-live-updates' disabled>
-        //         No live games right now
-        //     </button>`;
-        //     allGamesBoxDiv.appendChild(buttonLiveUpdates);
-
-        //     clearInterval(localStorage.getItem('localStInterval'));
-        // }
-
-    }).catch(err => {
-        console.log(err)
+      gamesRefresh(btnLiveUpdates, btnNoLiveUpdates, round);
+    })
+    .catch((err) => {
+      console.log(err);
     });
 };
 
